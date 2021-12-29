29a7363d42d505b73aa6e7f11bf4ed3b25c2983a
krlosMata <krlosmata@gmail.com>
2021-12-29 17:28:19 +0100

    WIP push image docker-hub

--- docker/scripts/deploy-docker.sh
+++ docker/scripts/deploy-docker.sh
@@ -2,9 +2,10 @@
 sudo rm -rf docker/gethData/geth_data
 DEV_PERIOD=0 docker-compose -f docker/docker-compose.geth.yml up -d geth
 sleep 5
-node docker/scripts/fund-accounts.js 
+node docker/scripts/fund-accounts.js
 npx hardhat run deployment/testnet/deployPoETestnet.js --network localhost
 mkdir docker/deploymentOutput
 mv deployment/testnet/deploy_output.json docker/deploymentOutput
 docker-compose -f docker/docker-compose.geth.yml down
+ls
 sudo docker build -t hermeznetwork/geth-zkevm-contracts -f docker/Dockerfile.geth .
