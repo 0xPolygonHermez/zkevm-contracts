6d1448287e4ed67e33cd74c4c3a490b3f360e32a
invocamanman <jesus.ligero@yahoo.es>
2022-01-24 14:29:30 +0100

    first integration with real prover

---
+++ docs/lib/Verifier.md
@@ -0,0 +1,34 @@
+
+
+
+## Functions
+### verifyingKey
+```solidity
+  function verifyingKey(
+  ) internal returns (struct Verifier.VerifyingKey vk)
+```
+
+
+
+
+### verify
+```solidity
+  function verify(
+  ) internal returns (uint256)
+```
+
+
+
+
+### verifyProof
+```solidity
+  function verifyProof(
+  ) public returns (bool r)
+```
+
+
+
+#### Return Values:
+| Name                           | Type          | Description                                                                  |
+| :----------------------------- | :------------ | :--------------------------------------------------------------------------- |
+|`r`| uint256[2] |  bool true if proof is valid
---
+++ test/contracts/real-prover/real-prover-test-inputs.test.js
@@ -0,0 +1,91 @@
+const { expect } = require('chai');
+const { ethers } = require('hardhat');
+
+const proofJson = require('./test-inputs/proof.json');
+const publicJson = require('./test-inputs/public.json');
+
+describe('Proof of efficiency', () => {
+    let deployer;
+    let sequencer;
+
+    let verifierContract;
+    let bridgeContract;
+    let proofOfEfficiencyContract;
+    let maticTokenContract;
+
+    const maticTokenName = 'Matic Token';
+    const maticTokenSymbol = 'MATIC';
+    const maticTokenInitialBalance = ethers.utils.parseEther('20000000');
+
+    const genesisRoot = '0x0000000000000000000000000000000000000000000000000000000000000000';
+
+    beforeEach('Deploy contract', async () => {
+        // load signers
+        [deployer, sequencer] = await ethers.getSigners();
+
+        // deploy mock verifier
+        const VerifierFactory = await ethers.getContractFactory(
+            'Verifier',
+        );
+        verifierContract = await VerifierFactory.deploy();
+
+        // deploy MATIC
+        const maticTokenFactory = await ethers.getContractFactory('ERC20PermitMock');
+        maticTokenContract = await maticTokenFactory.deploy(
+            maticTokenName,
+            maticTokenSymbol,
+            deployer.address,
+            maticTokenInitialBalance,
+        );
+        await maticTokenContract.deployed();
+
+        // deploy bridge
+        const precalculatePoEAddress = await ethers.utils.getContractAddress(
+            { from: deployer.address, nonce: (await ethers.provider.getTransactionCount(deployer.address)) + 1 },
+        );
+        const BridgeFactory = await ethers.getContractFactory('Bridge');
+        bridgeContract = await BridgeFactory.deploy(precalculatePoEAddress);
+        await bridgeContract.deployed();
+
+        // deploy proof of efficiency
+        const ProofOfEfficiencyFactory = await ethers.getContractFactory('ProofOfEfficiencyMock');
+        proofOfEfficiencyContract = await ProofOfEfficiencyFactory.deploy(
+            bridgeContract.address,
+            maticTokenContract.address,
+            verifierContract.address,
+            genesisRoot,
+        );
+        await proofOfEfficiencyContract.deployed();
+        expect(proofOfEfficiencyContract.address).to.be.equal(precalculatePoEAddress);
+
+        // fund sequencer address with Matic tokens
+        await maticTokenContract.transfer(sequencer.address, ethers.utils.parseEther('100'));
+    });
+
+    it('Test real prover', async () => {
+        const proofA = [proofJson.pi_a[0],
+            proofJson.pi_a[1],
+        ];
+        const proofB = [
+            [
+                proofJson.pi_b[0][1],
+                proofJson.pi_b[0][0],
+            ],
+            [
+                proofJson.pi_b[1][1],
+                proofJson.pi_b[1][0],
+            ],
+        ];
+        const proofC = [proofJson.pi_c[0],
+            proofJson.pi_c[1],
+        ];
+        const input = publicJson;
+
+        expect(await verifierContract.verifyProof(
+            proofA,
+            proofB,
+            proofC,
+            input,
+        )).to.be.equal(true);
+    });
+});
