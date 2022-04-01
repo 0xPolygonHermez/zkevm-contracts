b3b0bb146eb3f28d67c9b229ae6d5e120bcde0d0
invocamanman <jesus.ligero@yahoo.es>
2022-04-01 13:37:20 +0200

    add genesis to docker deployment

--- deployment/testnet/deployPoETestnet.js
+++ deployment/testnet/deployPoETestnet.js
@@ -5,6 +5,7 @@ const { Scalar } = require('ffjavascript');
 
 const pathOutputJson = path.join(__dirname, './deploy_output.json');
 const { expect } = require('chai');
+const genesis = require("./genesis.json")
 
 async function main() {
     const deployer = (await ethers.getSigners())[0];
@@ -66,23 +67,8 @@ async function main() {
     /*
         Deploy proof of efficiency
     */
-
-    /* "genesis": [
-    *    {
-    *      "address": "0x617b3a3528F9cDd6630fd3301B9c8911F7Bf063D",
-    *      "pvtKey": "0x28b2b0318721be8c8339199172cd7cc8f5e273800a35616ec893083a4b32c02e",
-    *      "balance": "100000000000000000000",
-    *      "nonce": "0"
-    *    },
-    *    {
-    *      "address": "0x4d5Cf5032B2a844602278b01199ED191A86c93ff",
-    *      "pvtKey": "0x4d27a600dce8c29b7bd080e29a26972377dbb04d7a27d919adbb602bf13cfd23",
-    *      "balance": "200000000000000000000",
-    *      "nonce": "0"
-    *    }
-    *  ] 
-    */
-    const genesisRootHex = `0x${Scalar.e("4091651772388093439828475955668620102367778455436412389529460210592290187513").toString(16).padStart(64, '0')}`;
+    // Check genesis file
+    const genesisRootHex = genesis.root;
 
     console.log('\n#######################');
     console.log('##### Deployment Proof of Efficiency #####');
