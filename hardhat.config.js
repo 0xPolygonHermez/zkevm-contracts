9097f1a4d2938e99b9839fbc0c2f5f35d97f7b8a
krlosMata <krlosmata@gmail.com>
2022-06-03 12:37:20 +0200

    fix versions packages + update genesis.json

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,19 +2,10 @@ require("dotenv").config();
 require("@nomiclabs/hardhat-waffle");
 require("hardhat-gas-reporter");
 require("solidity-coverage");
-require("@openzeppelin/hardhat-upgrades");
 require("@nomiclabs/hardhat-etherscan");
+require("@openzeppelin/hardhat-upgrades");
 
 DEFAULT_MNEMONIC = "test test test test test test test test test test test junk";
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async () => {
-  const accounts = await ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
