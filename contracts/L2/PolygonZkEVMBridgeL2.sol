eb7818a7e6175d673497adb8a6ea544eb3dd7e91
invocamanman <jesus.ligero@yahoo.es>
2023-09-12 16:38:53 +0200

    update version

--- contracts/L2/PolygonZkEVMBridgeL2.sol
+++ contracts/L2/PolygonZkEVMBridgeL2.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: AGPL-3.0
 
-pragma solidity 0.8.17;
+pragma solidity 0.8.20;
 
 import "../lib/DepositContract.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/utils/SafeERC20Upgradeable.sol";
--- contracts/L2/WETHzkEVM.sol
+++ contracts/L2/WETHzkEVM.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0
 // Implementation of permit based on https://github.com/WETH10/WETH10/blob/main/contracts/WETH10.sol
-pragma solidity 0.8.17;
+pragma solidity 0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
