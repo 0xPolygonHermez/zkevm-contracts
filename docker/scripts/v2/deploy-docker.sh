3d44fe5150ce4cf564ed4ebe169685beb0e8a581
invocamanman <jesus.ligero@yahoo.es>
2023-12-19 11:45:29 +0100

    fix cp docker

--- docker/scripts/v2/deploy-docker.sh
+++ docker/scripts/v2/deploy-docker.sh
@@ -4,7 +4,7 @@ DEV_PERIOD=1 docker-compose -f docker/docker-compose.geth.yml up -d geth
 sleep 5
 node docker/scripts/fund-accounts.js
 cp docker/scripts/v2/deploy_parameters_docker.json deployment/v2/deploy_parameters.json
-cp docker/scripts/v2/create_rollup_parameters.json deployment/v2/create_rollup_parameters.json
+cp docker/scripts/v2/create_rollup_parameters_docker.json deployment/v2/create_rollup_parameters.json
 npm run deploy:testnet:v2:localhost
 mkdir docker/deploymentOutput
 mv deployment/v2/deploy_output.json docker/deploymentOutput
