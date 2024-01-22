d75ad81738712190d329b9594dee62836824576d
invocamanman <jesus.ligero@yahoo.es>
2024-01-22 17:11:11 +0100

    upgrade scripts

---
+++ upgrade/upgradeToV2/upgradeToV2.ts
@@ -0,0 +1,332 @@
+/* eslint-disable no-await-in-loop, no-use-before-define, no-lonely-if */
+/* eslint-disable no-console, no-inner-declarations, no-undef, import/no-unresolved */
+import {expect} from "chai";
+import path = require("path");
+import fs = require("fs");
+
+import * as dotenv from "dotenv";
+dotenv.config({path: path.resolve(__dirname, "../../.env")});
+import {ethers, upgrades} from "hardhat";
+
+const pathOutputJson = path.join(__dirname, "./upgrade_output.json");
+
+const deployParameters = require("./deploy_parameters.json");
+const deployOutputParameters = require("./deploy_output.json");
+const upgradeParameters = require("./upgrade_parameters.json");
+
+const pathOZUpgradability = path.join(__dirname, `../../.openzeppelin/${process.env.HARDHAT_NETWORK}.json`);
+
+async function main() {
+    const {
+        admin,
+        trustedAggregator,
+        trustedAggregatorTimeout,
+        pendingStateTimeout,
+        zkEVMOwner,
+        trustedSequencer,
+        chainID,
+    } = deployParameters;
+    const emergencyCouncilAddress = zkEVMOwner;
+
+    //polTokenAddress TODO
+    const {realVerifier, newForkID, timelockDelay} = upgradeParameters;
+
+    const salt = upgradeParameters.timelockSalt || ethers.ZeroHash;
+
+    const currentBridgeAddress = deployOutputParameters.polygonZkEVMBridgeAddress;
+    const currentGlobalExitRootAddress = deployOutputParameters.polygonZkEVMGlobalExitRootAddress;
+    const currentPolygonZkEVMAddress = deployOutputParameters.polygonZkEVMAddress;
+    const currentTimelockAddress = deployOutputParameters.timelockContractAddress;
+    console.log({currentBridgeAddress});
+    // Load provider
+    let currentProvider = ethers.provider;
+    if (deployParameters.multiplierGas || deployParameters.maxFeePerGas) {
+        if (process.env.HARDHAT_NETWORK !== "hardhat") {
+            currentProvider = ethers.getDefaultProvider(
+                `https://${process.env.HARDHAT_NETWORK}.infura.io/v3/${process.env.INFURA_PROJECT_ID}`
+            ) as any;
+            if (deployParameters.maxPriorityFeePerGas && deployParameters.maxFeePerGas) {
+                console.log(
+                    `Hardcoded gas used: MaxPriority${deployParameters.maxPriorityFeePerGas} gwei, MaxFee${deployParameters.maxFeePerGas} gwei`
+                );
+                const FEE_DATA = new ethers.FeeData(
+                    null,
+                    ethers.parseUnits(deployParameters.maxFeePerGas, "gwei"),
+                    ethers.parseUnits(deployParameters.maxPriorityFeePerGas, "gwei")
+                );
+
+                currentProvider.getFeeData = async () => FEE_DATA;
+            } else {
+                console.log("Multiplier gas used: ", deployParameters.multiplierGas);
+                async function overrideFeeData() {
+                    const feedata = await ethers.provider.getFeeData();
+                    return new ethers.FeeData(
+                        null,
+                        ((feedata.maxFeePerGas as bigint) * BigInt(deployParameters.multiplierGas)) / 1000n,
+                        ((feedata.maxPriorityFeePerGas as bigint) * BigInt(deployParameters.multiplierGas)) / 1000n
+                    );
+                }
+                currentProvider.getFeeData = overrideFeeData;
+            }
+        }
+    }
+
+    // Load deployer
+    let deployer;
+    if (deployParameters.deployerPvtKey) {
+        deployer = new ethers.Wallet(deployParameters.deployerPvtKey, currentProvider);
+    } else if (process.env.MNEMONIC) {
+        deployer = ethers.HDNodeWallet.fromMnemonic(
+            ethers.Mnemonic.fromPhrase(process.env.MNEMONIC),
+            "m/44'/60'/0'/0/0"
+        ).connect(currentProvider);
+    } else {
+        [deployer] = await ethers.getSigners();
+    }
+
+    console.log("deploying with: ", deployer.address);
+
+    const proxyAdmin = await upgrades.admin.getInstance();
+
+    // // coudl be done qiwth deterministic deployment TODO
+    // // Load zkEVM deployer
+    // const PolgonZKEVMDeployerFactory = await ethers.getContractFactory("PolygonZkEVMDeployer", deployer);
+    // const zkEVMDeployerContract = PolgonZKEVMDeployerFactory.attach(zkEVMDeployerAddress) as PolygonZkEVMDeployer;
+
+    // // check deployer is the owner of the deployer
+    // if ((await deployer.provider?.getCode(zkEVMDeployerContract.target)) === "0x") {
+    //     throw new Error("zkEVM deployer contract is not deployed");
+    // }
+
+    // TODO move to another script deploy Pol Token
+    /*
+     *Deployment pol
+     */
+    const polTokenName = "Pol Token";
+    const polTokenSymbol = "POL";
+    const polTokenInitialBalance = ethers.parseEther("20000000");
+
+    const polTokenFactory = await ethers.getContractFactory("ERC20PermitMock", deployer);
+    const polTokenContract = await polTokenFactory.deploy(
+        polTokenName,
+        polTokenSymbol,
+        deployer.address,
+        polTokenInitialBalance
+    );
+    await polTokenContract.waitForDeployment();
+
+    console.log("#######################\n");
+    console.log("pol deployed to:", polTokenContract.target);
+    console.log("you can verify the new impl address with:");
+    console.log(
+        `npx hardhat verify --constructor-args upgrade/arguments.js ${polTokenContract.target} --network ${process.env.HARDHAT_NETWORK}\n`
+    );
+    console.log("Copy the following constructor arguments on: upgrade/arguments.js \n", [
+        polTokenName,
+        polTokenSymbol,
+        deployer.address,
+        polTokenInitialBalance,
+    ]);
+
+    const tokensBalance = ethers.parseEther("100000");
+    await (await polTokenContract.transfer(trustedSequencer, tokensBalance)).wait();
+
+    const polTokenAddress = polTokenContract.target;
+
+    // deploy new verifier
+    let verifierContract;
+    if (realVerifier === true) {
+        const VerifierRollup = await ethers.getContractFactory("FflonkVerifier", deployer);
+        verifierContract = await VerifierRollup.deploy();
+        await verifierContract.waitForDeployment();
+    } else {
+        const VerifierRollupHelperFactory = await ethers.getContractFactory("VerifierRollupHelperMock", deployer);
+        verifierContract = await VerifierRollupHelperFactory.deploy();
+        await verifierContract.waitForDeployment();
+    }
+    console.log("#######################\n");
+    console.log("Verifier deployed to:", verifierContract.target);
+    console.log(`npx hardhat verify ${verifierContract.target} --network ${process.env.HARDHAT_NETWORK}`);
+
+    // load timelock
+    const timelockContractFactory = await ethers.getContractFactory("PolygonZkEVMTimelock", deployer);
+    const timelockContract = timelockContractFactory.attach(currentTimelockAddress);
+
+    // prapare upgrades
+
+    // Prepare Upgrade PolygonZkEVMBridge
+    const polygonZkEVMBridgeFactory = await ethers.getContractFactory("PolygonZkEVMBridgeV2", deployer);
+
+    const newBridgeImpl = await upgrades.prepareUpgrade(currentBridgeAddress, polygonZkEVMBridgeFactory, {
+        unsafeAllow: ["constructor"],
+    });
+
+    console.log("#######################\n");
+    console.log(`PolygonZkEVMBridge impl: ${newBridgeImpl}`);
+
+    console.log("you can verify the new impl address with:");
+    console.log(`npx hardhat verify ${newBridgeImpl} --network ${process.env.HARDHAT_NETWORK}`);
+
+    const operationBridge = genOperation(
+        proxyAdmin.target,
+        0, // value
+        proxyAdmin.interface.encodeFunctionData("upgrade", [currentBridgeAddress, newBridgeImpl]),
+        ethers.ZeroHash, // predecesoor
+        salt // salt
+    );
+
+    // prepare upgrade global exit root
+    // Prepare Upgrade  PolygonZkEVMBridge
+    const polygonGlobalExitRootV2 = await ethers.getContractFactory("PolygonZkEVMGlobalExitRootV2", deployer);
+
+    const newGlobalExitRoortImpl = await upgrades.prepareUpgrade(
+        currentGlobalExitRootAddress,
+        polygonGlobalExitRootV2,
+        {
+            constructorArgs: [currentPolygonZkEVMAddress, currentBridgeAddress],
+            unsafeAllow: ["constructor", "state-variable-immutable"],
+        }
+    );
+
+    console.log("#######################\n");
+    console.log(`polygonGlobalExitRootV2 impl: ${newGlobalExitRoortImpl}`);
+
+    console.log("you can verify the new impl address with:");
+    console.log(
+        `npx hardhat verify --constructor-args upgrade/arguments.js ${newGlobalExitRoortImpl} --network ${process.env.HARDHAT_NETWORK}\n`
+    );
+    console.log("Copy the following constructor arguments on: upgrade/arguments.js \n", [
+        currentPolygonZkEVMAddress,
+        currentBridgeAddress,
+    ]);
+
+    const operationGlobalExitRoot = genOperation(
+        proxyAdmin.target,
+        0, // value
+        proxyAdmin.interface.encodeFunctionData("upgrade", [currentGlobalExitRootAddress, newGlobalExitRoortImpl]),
+        ethers.ZeroHash, // predecesoor
+        salt // salt
+    );
+
+    // Update current system to rollup manager
+
+    // Deploy new polygonZkEVM
+    const PolygonZkEVMV2ExistentFactory = await ethers.getContractFactory("PolygonZkEVMExistentEtrog");
+    const newPolygonZkEVMContract = (await upgrades.deployProxy(PolygonZkEVMV2ExistentFactory, [], {
+        initializer: false,
+        constructorArgs: [
+            currentGlobalExitRootAddress,
+            polTokenAddress,
+            currentBridgeAddress,
+            currentPolygonZkEVMAddress,
+        ],
+        unsafeAllow: ["constructor", "state-variable-immutable"],
+    })) as any;
+
+    console.log("#######################\n");
+    console.log(`new PolygonZkEVM: ${newPolygonZkEVMContract.target}`);
+
+    console.log("you can verify the new impl address with:");
+    console.log(
+        `npx hardhat verify --constructor-args upgrade/arguments.js ${newPolygonZkEVMContract.target} --network ${process.env.HARDHAT_NETWORK}\n`
+    );
+    console.log("Copy the following constructor arguments on: upgrade/arguments.js \n", [
+        currentGlobalExitRootAddress,
+        polTokenAddress,
+        currentBridgeAddress,
+        currentPolygonZkEVMAddress,
+    ]);
+
+    // Upgrade to rollup manager previous polygonZKEVM
+    const PolygonRollupManagerFactory = await ethers.getContractFactory("PolygonRollupManager");
+    const implRollupManager = await upgrades.prepareUpgrade(currentPolygonZkEVMAddress, PolygonRollupManagerFactory, {
+        constructorArgs: [currentGlobalExitRootAddress, polTokenAddress, currentBridgeAddress],
+        unsafeAllow: ["constructor", "state-variable-immutable"],
+    });
+
+    console.log("#######################\n");
+    console.log(`Polygon rollup manager: ${implRollupManager}`);
+
+    console.log("you can verify the new impl address with:");
+    console.log(
+        `npx hardhat verify --constructor-args upgrade/arguments.js ${implRollupManager} --network ${process.env.HARDHAT_NETWORK}\n`
+    );
+    console.log("Copy the following constructor arguments on: upgrade/arguments.js \n", [
+        currentGlobalExitRootAddress,
+        polTokenAddress,
+        currentBridgeAddress,
+    ]);
+
+    const operationRollupManager = genOperation(
+        proxyAdmin.target,
+        0, // value
+        proxyAdmin.interface.encodeFunctionData("upgradeAndCall", [
+            currentPolygonZkEVMAddress,
+            implRollupManager,
+            PolygonRollupManagerFactory.interface.encodeFunctionData("initialize", [
+                trustedAggregator,
+                pendingStateTimeout,
+                trustedAggregatorTimeout,
+                admin,
+                timelockContract.target,
+                emergencyCouncilAddress,
+                newPolygonZkEVMContract.target,
+                verifierContract.target,
+                newForkID,
+                chainID,
+            ]),
+        ]),
+        ethers.ZeroHash, // predecesoor
+        salt // salt
+    );
+
+    // Schedule operation
+    const scheduleData = timelockContractFactory.interface.encodeFunctionData("scheduleBatch", [
+        [operationGlobalExitRoot.target, operationBridge.target, operationRollupManager.target],
+        [operationGlobalExitRoot.value, operationBridge.value, operationRollupManager.value],
+        [operationGlobalExitRoot.data, operationBridge.data, operationRollupManager.data],
+        ethers.ZeroHash, // predecesoor
+        salt, // salt
+        timelockDelay,
+    ]);
+
+    // Execute operation
+    const executeData = timelockContractFactory.interface.encodeFunctionData("executeBatch", [
+        [operationGlobalExitRoot.target, operationBridge.target, operationRollupManager.target],
+        [operationGlobalExitRoot.value, operationBridge.value, operationRollupManager.value],
+        [operationGlobalExitRoot.data, operationBridge.data, operationRollupManager.data],
+        ethers.ZeroHash, // predecesoor
+        salt, // salt
+    ]);
+
+    console.log({scheduleData});
+    console.log({executeData});
+
+    const outputJson = {
+        scheduleData,
+        executeData,
+        verifierAddress: verifierContract.target,
+    };
+    fs.writeFileSync(pathOutputJson, JSON.stringify(outputJson, null, 1));
+}
+
+main().catch((e) => {
+    console.error(e);
+    process.exit(1);
+});
+
+// OZ test functions
+function genOperation(target: any, value: any, data: any, predecessor: any, salt: any) {
+    const id = ethers.solidityPackedKeccak256(
+        ["address", "uint256", "bytes", "uint256", "bytes32"],
+        [target, value, data, predecessor, salt]
+    );
+    return {
+        id,
+        target,
+        value,
+        data,
+        predecessor,
+        salt,
+    };
+}
