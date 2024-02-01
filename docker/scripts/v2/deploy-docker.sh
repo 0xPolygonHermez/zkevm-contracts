f0c48ce20dec87e17be4f8c9cef0d93fe794e81b
begmaroman <begmaroman@gmail.com>
2024-02-01 14:49:56 +0000

    Added deployment output files to the container and serve them via nginx

--- docker/scripts/v2/deploy-docker.sh
+++ docker/scripts/v2/deploy-docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo rm -rf docker/gethData/geth_data
-DEV_PERIOD=1 docker-compose -f docker/docker-compose.geth.yml up -d geth
+DEV_PERIOD=1 docker-compose -f docker/docker-compose.yml up -d geth
 sleep 5
 node docker/scripts/fund-accounts.js
 cp docker/scripts/v2/deploy_parameters_docker.json deployment/v2/deploy_parameters.json
@@ -10,7 +10,7 @@ mkdir docker/deploymentOutput
 mv deployment/v2/deploy_output.json docker/deploymentOutput
 mv deployment/v2/genesis.json docker/deploymentOutput
 mv deployment/v2/create_rollup_output.json docker/deploymentOutput
-DEV_PERIOD=1 docker-compose -f docker/docker-compose.geth.yml down
-sudo docker build -t hermeznetwork/geth-zkevm-contracts -f docker/Dockerfile.geth .
+DEV_PERIOD=1 docker-compose -f docker/docker-compose.yml down
+docker build -t hermeznetwork/geth-zkevm-contracts -f docker/Dockerfile --progress .
 # Let it readable for the multiplatform build coming later!
 sudo chmod -R go+rxw docker/gethData
