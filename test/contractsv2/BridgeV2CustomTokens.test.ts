da27acf0b7858dfad15fa3dca69db9fcc8f8b291
pyk <gm@pyk.sh>
2024-03-20 10:05:23 +0000

    chore: should claim non-local tokens with default wrapper

--- test/contractsv2/BridgeV2CustomTokens.test.ts
+++ test/contractsv2/BridgeV2CustomTokens.test.ts
@@ -9,7 +9,12 @@ import {
     MTBridge as MerkleTreeBridge,
     mtBridgeUtils,
 } from "@0xpolygonhermez/zkevm-commonjs";
-import {type PolygonZkEVMGlobalExitRootV2, type PolygonZkEVMBridgeV2, ERC20PermitMock} from "../../typechain-types";
+import {
+    type PolygonZkEVMGlobalExitRootV2,
+    type PolygonZkEVMBridgeV2,
+    type ERC20PermitMock,
+    type TokenWrapped,
+} from "../../typechain-types";
 
 const _GLOBAL_INDEX_MAINNET_FLAG = 2n ** 64n;
 const {verifyMerkleProof, getLeafValue} = mtBridgeUtils;
@@ -34,6 +39,11 @@ describe("PolygonZkEVMBridgeV2: Custom Tokens", () => {
     let polygonZkEVMBridge: PolygonZkEVMBridgeV2;
     let polygonZkEVMGlobalExitRoot: PolygonZkEVMGlobalExitRootV2;
     let token: ERC20PermitMock;
+
+    const tokenMetadata = ethers.AbiCoder.defaultAbiCoder().encode(
+        ["string", "string", "uint8"],
+        ["Polygon", "POL", 18]
+    );
 
     const networkId = 0;
     const LEAF_TYPE_ASSET = 0;
@@ -87,11 +97,7 @@ describe("PolygonZkEVMBridgeV2: Custom Tokens", () => {
         const destinationNetworkId = networkId;
         const destinationAddress = alice.address;
         const amount = ethers.parseEther("1.0");
-        const metadata = ethers.AbiCoder.defaultAbiCoder().encode(
-            ["string", "string", "uint8"],
-            ["Polygon", "POL", 18]
-        );
-        const metadataHash = ethers.solidityPackedKeccak256(["bytes"], [metadata]);
+        const metadataHash = ethers.solidityPackedKeccak256(["bytes"], [tokenMetadata]);
 
         const leafValue = getLeafValue(
             LEAF_TYPE_ASSET,
@@ -138,13 +144,6 @@ describe("PolygonZkEVMBridgeV2: Custom Tokens", () => {
         const bridgeBalance = ethers.parseEther("1.0");
         await setBalance(polygonZkEVMBridge.target.toString(), bridgeBalance);
 
-        // const balance = await ethers.provider.getBalance(polygonZkEVMBridge.target);
-
-        // transfer tokens, then claim
-        // await expect(gasToken.transfer(polygonZkEVMBridge.target, amount))
-        //     .to.emit(gasToken, "Transfer")
-        //     .withArgs(deployer.address, polygonZkEVMBridge.target, amount);
-
         // 5. Alice claim
         const beforeClaim = await ethers.provider.getBalance(alice.address);
         await polygonZkEVMBridge.claimAsset(
@@ -158,7 +157,7 @@ describe("PolygonZkEVMBridgeV2: Custom Tokens", () => {
             destinationNetworkId,
             destinationAddress,
             amount,
-            metadata
+            tokenMetadata
         );
         const afterClaim = await ethers.provider.getBalance(alice.address);
         const aliceBalance = afterClaim - beforeClaim;
@@ -177,11 +176,7 @@ describe("PolygonZkEVMBridgeV2: Custom Tokens", () => {
         const destinationNetworkId = networkId;
         const destinationAddress = alice.address;
         const amount = ethers.parseEther("1.0");
-        const metadata = ethers.AbiCoder.defaultAbiCoder().encode(
-            ["string", "string", "uint8"],
-            ["Polygon", "POL", 18]
-        );
-        const metadataHash = ethers.solidityPackedKeccak256(["bytes"], [metadata]);
+        const metadataHash = ethers.solidityPackedKeccak256(["bytes"], [tokenMetadata]);
 
         const leafValue = getLeafValue(
             LEAF_TYPE_ASSET,
@@ -227,13 +222,6 @@ describe("PolygonZkEVMBridgeV2: Custom Tokens", () => {
         // 4. Topup the bridge
         await token.mint(polygonZkEVMBridge.target, amount);
 
-        // const balance = await ethers.provider.getBalance(polygonZkEVMBridge.target);
-
-        // transfer tokens, then claim
-        // await expect(gasToken.transfer(polygonZkEVMBridge.target, amount))
-        //     .to.emit(gasToken, "Transfer")
-        //     .withArgs(deployer.address, polygonZkEVMBridge.target, amount);
-
         // 5. Alice claim
         const beforeClaim = await token.balanceOf(alice.address);
         await polygonZkEVMBridge.claimAsset(
@@ -247,10 +235,194 @@ describe("PolygonZkEVMBridgeV2: Custom Tokens", () => {
             destinationNetworkId,
             destinationAddress,
             amount,
-            metadata
+            tokenMetadata
         );
         const afterClaim = await token.balanceOf(alice.address);
         const aliceBalance = afterClaim - beforeClaim;
         expect(aliceBalance).eq(amount);
+    });
+
+    it("should claim non-local tokens with default wrapper", async () => {
+        // 1. we need to setup the Sparse Merkle Tree proofs
+
+        // Local merkle tree
+        const height = 32;
+        const merkleTreeLocal = new MerkleTreeBridge(height);
+
+        const originNetworkId = networkId + 1; // NOTE: non-local tokens
+        const tokenAddress = token.target;
+        const destinationNetworkId = networkId;
+        const destinationAddress = alice.address;
+        const amount = ethers.parseEther("1.0");
+        const metadataHash = ethers.solidityPackedKeccak256(["bytes"], [tokenMetadata]);
+
+        const leafValue = getLeafValue(
+            LEAF_TYPE_ASSET,
+            originNetworkId,
+            tokenAddress,
+            destinationNetworkId,
+            destinationAddress,
+            amount,
+            metadataHash
+        );
+        merkleTreeLocal.add(leafValue);
+        const indexLocal = 0;
+        const proofLocal = merkleTreeLocal.getProofTreeByIndex(indexLocal);
+        const rootLocalRollup = merkleTreeLocal.getRoot();
+
+        // Double check the SMT Proof
+        expect(verifyMerkleProof(leafValue, proofLocal, indexLocal, rootLocalRollup)).to.be.equal(true);
+
+        // Rollup merkle tree
+        const merkleTreeRollup = new MerkleTreeBridge(height);
+        for (let i = 0; i < 10; i++) {
+            merkleTreeRollup.add(rootLocalRollup);
+        }
+        const indexRollup = 5;
+        const proofRollup = merkleTreeRollup.getProofTreeByIndex(indexRollup);
+        const rootRollup = merkleTreeRollup.getRoot();
+
+        // Double check the SMT Proof
+        expect(verifyMerkleProof(rootLocalRollup, proofRollup, indexRollup, rootRollup)).to.be.equal(true);
+
+        // 2. we need to update the exit root
+        const lastMainnetExitRoot = await polygonZkEVMGlobalExitRoot.lastMainnetExitRoot();
+        await expect(polygonZkEVMGlobalExitRoot.connect(rollupManager).updateExitRoot(rootRollup))
+            .to.emit(polygonZkEVMGlobalExitRoot, "UpdateL1InfoTree")
+            .withArgs(lastMainnetExitRoot, rootRollup);
+
+        const lastRollupExitRoot = await polygonZkEVMGlobalExitRoot.lastRollupExitRoot();
+        expect(lastRollupExitRoot).to.be.equal(rootRollup);
+
+        // 3. we need to calculate the global index
+        const globalIndex = computeGlobalIndex(indexLocal, indexRollup, false);
+
+        // 4. we need to get the address of token wrapper
+        const tokenWrappedFactory = await ethers.getContractFactory("TokenWrapped");
+        // create2 parameters
+        const salt = ethers.solidityPackedKeccak256(["uint32", "address"], [originNetworkId, tokenAddress]);
+        const minimalBytecodeProxy = await polygonZkEVMBridge.BASE_INIT_BYTECODE_WRAPPED_TOKEN();
+        const hashInitCode = ethers.solidityPackedKeccak256(["bytes", "bytes"], [minimalBytecodeProxy, tokenMetadata]);
+        const precalculateWrappedErc20 = await ethers.getCreate2Address(
+            polygonZkEVMBridge.target as string,
+            salt,
+            hashInitCode
+        );
+        const defaultWrappedToken = tokenWrappedFactory.attach(precalculateWrappedErc20) as TokenWrapped;
+
+        // 5. Alice claim
+        await expect(
+            polygonZkEVMBridge.claimAsset(
+                proofLocal,
+                proofRollup,
+                globalIndex,
+                lastMainnetExitRoot,
+                lastRollupExitRoot,
+                originNetworkId,
+                tokenAddress,
+                destinationNetworkId,
+                destinationAddress,
+                amount,
+                tokenMetadata
+            )
+        )
+            .to.emit(polygonZkEVMBridge, "ClaimEvent")
+            .withArgs(globalIndex, originNetworkId, tokenAddress, destinationAddress, amount)
+            .to.emit(polygonZkEVMBridge, "NewWrappedToken")
+            .withArgs(originNetworkId, tokenAddress, precalculateWrappedErc20, tokenMetadata)
+            .to.emit(defaultWrappedToken, "Transfer")
+            .withArgs(ethers.ZeroAddress, destinationAddress, amount);
+    });
+
+    it("should claim non-local tokens with custom wrapper", async () => {
+        // 1. we need to setup the Sparse Merkle Tree proofs
+
+        // Local merkle tree
+        const height = 32;
+        const merkleTreeLocal = new MerkleTreeBridge(height);
+
+        const originNetworkId = networkId + 1; // NOTE: non-local tokens
+        const tokenAddress = token.target;
+        const destinationNetworkId = networkId;
+        const destinationAddress = alice.address;
+        const amount = ethers.parseEther("1.0");
+        const metadataHash = ethers.solidityPackedKeccak256(["bytes"], [tokenMetadata]);
+
+        const leafValue = getLeafValue(
+            LEAF_TYPE_ASSET,
+            originNetworkId,
+            tokenAddress,
+            destinationNetworkId,
+            destinationAddress,
+            amount,
+            metadataHash
+        );
+        merkleTreeLocal.add(leafValue);
+        const indexLocal = 0;
+        const proofLocal = merkleTreeLocal.getProofTreeByIndex(indexLocal);
+        const rootLocalRollup = merkleTreeLocal.getRoot();
+
+        // Double check the SMT Proof
+        expect(verifyMerkleProof(leafValue, proofLocal, indexLocal, rootLocalRollup)).to.be.equal(true);
+
+        // Rollup merkle tree
+        const merkleTreeRollup = new MerkleTreeBridge(height);
+        for (let i = 0; i < 10; i++) {
+            merkleTreeRollup.add(rootLocalRollup);
+        }
+        const indexRollup = 5;
+        const proofRollup = merkleTreeRollup.getProofTreeByIndex(indexRollup);
+        const rootRollup = merkleTreeRollup.getRoot();
+
+        // Double check the SMT Proof
+        expect(verifyMerkleProof(rootLocalRollup, proofRollup, indexRollup, rootRollup)).to.be.equal(true);
+
+        // 2. we need to update the exit root
+        const lastMainnetExitRoot = await polygonZkEVMGlobalExitRoot.lastMainnetExitRoot();
+        await expect(polygonZkEVMGlobalExitRoot.connect(rollupManager).updateExitRoot(rootRollup))
+            .to.emit(polygonZkEVMGlobalExitRoot, "UpdateL1InfoTree")
+            .withArgs(lastMainnetExitRoot, rootRollup);
+
+        const lastRollupExitRoot = await polygonZkEVMGlobalExitRoot.lastRollupExitRoot();
+        expect(lastRollupExitRoot).to.be.equal(rootRollup);
+
+        // 3. we need to calculate the global index
+        const globalIndex = computeGlobalIndex(indexLocal, indexRollup, false);
+
+        // 4. we need to get the address of token wrapper
+        const tokenWrappedFactory = await ethers.getContractFactory("TokenWrapped");
+        // create2 parameters
+        const salt = ethers.solidityPackedKeccak256(["uint32", "address"], [originNetworkId, tokenAddress]);
+        const minimalBytecodeProxy = await polygonZkEVMBridge.BASE_INIT_BYTECODE_WRAPPED_TOKEN();
+        const hashInitCode = ethers.solidityPackedKeccak256(["bytes", "bytes"], [minimalBytecodeProxy, tokenMetadata]);
+        const precalculateWrappedErc20 = await ethers.getCreate2Address(
+            polygonZkEVMBridge.target as string,
+            salt,
+            hashInitCode
+        );
+        const defaultWrappedToken = tokenWrappedFactory.attach(precalculateWrappedErc20) as TokenWrapped;
+
+        // 5. Alice claim
+        await expect(
+            polygonZkEVMBridge.claimAsset(
+                proofLocal,
+                proofRollup,
+                globalIndex,
+                lastMainnetExitRoot,
+                lastRollupExitRoot,
+                originNetworkId,
+                tokenAddress,
+                destinationNetworkId,
+                destinationAddress,
+                amount,
+                tokenMetadata
+            )
+        )
+            .to.emit(polygonZkEVMBridge, "ClaimEvent")
+            .withArgs(globalIndex, originNetworkId, tokenAddress, destinationAddress, amount)
+            .to.emit(polygonZkEVMBridge, "NewWrappedToken")
+            .withArgs(originNetworkId, tokenAddress, precalculateWrappedErc20, tokenMetadata)
+            .to.emit(defaultWrappedToken, "Transfer")
+            .withArgs(ethers.ZeroAddress, destinationAddress, amount);
     });
 });
