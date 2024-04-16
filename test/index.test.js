38ec6a9f98a4c94285541eaee7de1a6414714495
DhairyaSethi <55102840+DhairyaSethi@users.noreply.github.com>
2024-04-16 13:09:25 +0530

    chore: verify trusted_setup integrity

--- test/index.test.js
+++ test/index.test.js
@@ -1,12 +1,22 @@
 /* eslint-disable no-restricted-syntax */
-const { expect } = require('chai');
-const index = require('../index');
+const {expect} = require("chai");
+const axios = require("axios");
+const {readFileSync: rfs} = require("fs");
+const {join} = require("path");
+const index = require("../index");
 
-describe('should export index', () => {
-    it('should validate all abi exports', async () => {
+describe("should export index", () => {
+    it("should validate all abi exports", async () => {
         for (const file of Object.keys(index)) {
-            expect(index[file]).to.be.an('object');
-            expect(index[file]).to.have.property('abi');
+            expect(index[file]).to.be.an("object");
+            expect(index[file]).to.have.property("abi");
         }
+    });
+    it("should check kzg trusted_setup", async () => {
+        const officialTrustedSetupSrc =
+            "https://raw.githubusercontent.com/ethereum/c-kzg-4844/main/src/trusted_setup.txt";
+        const officialTrustedSetup = rfs(join(__dirname, "utils", "trusted_setup.txt"), "utf-8");
+        const response = await axios.get(officialTrustedSetupSrc);
+        expect(response.data).to.equal(officialTrustedSetup);
     });
 });
