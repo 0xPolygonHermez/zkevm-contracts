200a788279a7c52cd5bde1fc323fccd9932b1ad1
Laia Soler <laia@polygon.technology>
2024-07-15 15:54:58 +0200

    review PR

--- contracts/v2/consensus/pessimistic/PolygonConsensusContract.sol
+++ contracts/v2/consensus/pessimistic/PolygonConsensusContract.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: AGPL-3.0
 pragma solidity ^0.8.20;
 
-import "./PolygonConsensusBase.sol";
+import "../../lib/PolygonConsensusBase.sol";
 
 abstract contract PolygonConsensusContract is
     PolygonConsensusBase
