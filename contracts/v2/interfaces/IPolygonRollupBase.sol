d0e45f8315b7df2ef5e3aedb5c26d982c3ad01d7
invocamanman <jesus.ligero@yahoo.es>
2024-07-17 02:24:39 +0200

    ongoign

--- contracts/v2/interfaces/IPolygonRollupBase.sol
+++ contracts/v2/interfaces/IPolygonRollupBase.sol
@@ -2,23 +2,14 @@
 
 pragma solidity ^0.8.20;
 
-interface IPolygonRollupBase {
-    function initialize(
-        address _admin,
-        address sequencer,
-        uint32 networkID,
-        address gasTokenAddress,
-        string memory sequencerURL,
-        string memory _networkName
-    ) external;
+import "./IPolygonConsensusBase.sol";
 
+interface IPolygonRollupBase is IPolygonConsensusBase {
     function onVerifyBatches(
         uint64 lastVerifiedBatch,
         bytes32 newStateRoot,
         address aggregator
     ) external;
-
-    function admin() external returns (address);
 
     function rollbackBatches(
         uint64 targetBatch,
