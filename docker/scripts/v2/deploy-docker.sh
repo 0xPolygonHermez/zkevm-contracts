7473921d41c530d105c26db38e54556dbe8205c7
Ignasi <ignasi@polygon.technology>
2024-07-18 14:53:18 +0200

    Update events for PP

--- docker/scripts/v2/deploy-docker.sh
+++ docker/scripts/v2/deploy-docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo rm -rf docker/gethData/geth_data
-sudo DEV_PERIOD=1 docker-compose -f docker/docker-compose.yml up -d geth
+rm -rf docker/gethData/geth_data
+DEV_PERIOD=1 docker-compose -f docker/docker-compose.yml up -d geth
 sleep 5
 node docker/scripts/fund-accounts.js
 cp docker/scripts/v2/deploy_parameters_docker.json deployment/v2/deploy_parameters.json
@@ -13,4 +13,4 @@ sudo mv deployment/v2/create_rollup_output.json docker/deploymentOutput
 sudo DEV_PERIOD=1 docker-compose -f docker/docker-compose.yml down
 sudo docker build -t hermeznetwork/geth-zkevm-contracts -f docker/Dockerfile .
 # Let it readable for the multiplatform build coming later!
-sudo chmod -R go+rxw docker/gethData
+chmod -R go+rxw docker/gethData
