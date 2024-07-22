8ebb50d3203bb98752ae47d00f2f359fe709469a
Laia Soler <laia@polygon.technology>
2024-07-22 17:30:18 +0200

    fix dockerv2 sudo

--- docker/scripts/v2/deploy-dockerv2.sh
+++ docker/scripts/v2/deploy-dockerv2.sh
@@ -10,10 +10,10 @@ cp docker/scripts/v2/create_rollup_parameters_docker.json deployment/v2/create_r
 npm run deploy:testnet:v2:localhost
 rm -rf docker/deploymentOutput
 mkdir docker/deploymentOutput
-sudo mv deployment/v2/deploy_output.json docker/deploymentOutput
-sudo mv deployment/v2/genesis.json docker/deploymentOutput
-sudo mv deployment/v2/create_rollup_output.json docker/deploymentOutput
-sudo DEV_PERIOD=1 docker compose -f docker/docker-compose.yml down
+mv deployment/v2/deploy_output.json docker/deploymentOutput
+mv deployment/v2/genesis.json docker/deploymentOutput
+mv deployment/v2/create_rollup_output.json docker/deploymentOutput
+DEV_PERIOD=1 docker compose -f docker/docker-compose.yml down
 sudo docker build -t hermeznetwork/geth-zkevm-contracts -f docker/Dockerfile .
 # Let it readable for the multiplatform build coming later!
 sudo chmod -R go+rxw docker/gethData
