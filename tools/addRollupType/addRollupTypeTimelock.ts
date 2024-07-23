82f59ad067af015b7ea6f97eb97551b5f9478b24
krlosMata <krlosmata@gmail.com>
2024-07-23 12:29:00 +0200

    update docs + fox zeroAddress

--- tools/addRollupType/addRollupTypeTimelock.ts
+++ tools/addRollupType/addRollupTypeTimelock.ts
@@ -126,7 +126,7 @@ async function main() {
 
     // get bridge address in genesis file
     let genesisBridgeAddress = ethers.ZeroAddress;
-    for (let i = 0; i < genesis.genesis.length; i++) {
+    for (let i = 0; i < genesis.genesis.lenght; i++) {
         if (genesis.genesis[i].contractName === "PolygonZkEVMBridge proxy") {
             genesisBridgeAddress = genesis.genesis[i].address;
             break;
