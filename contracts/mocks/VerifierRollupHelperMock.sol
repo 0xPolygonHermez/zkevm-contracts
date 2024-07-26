a263e266a2c92fb2d0d3e1d5b65dc361701a7aa0
krlosMata <krlosmata@gmail.com>
2024-07-26 09:32:58 +0200

    test full pesismistic

--- contracts/mocks/VerifierRollupHelperMock.sol
+++ contracts/mocks/VerifierRollupHelperMock.sol
@@ -3,12 +3,20 @@
 pragma solidity 0.8.20;
 
 import "../interfaces/IVerifierRollup.sol";
+import "../v2/interfaces/ISP1Verifier.sol";
 
-contract VerifierRollupHelperMock is IVerifierRollup {
+contract VerifierRollupHelperMock is IVerifierRollup, ISP1Verifier {
     function verifyProof(
         bytes32[24] calldata proof,
         uint256[1] memory pubSignals
     ) public pure override returns (bool) {
         return true;
     }
+
+    // SP1 interface
+    function verifyProof(
+        bytes32 programVKey,
+        bytes calldata publicValues,
+        bytes calldata proofBytes
+    ) public pure {}
 }
