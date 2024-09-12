cf247a732ef534198c3d8166b145704893b6c8f2
Laia Soler <laia@polygon.technology>
2024-09-12 18:05:31 +0200

    fix build dockerv2:contracts

--- deployment/v2/3_deployContracts.ts
+++ deployment/v2/3_deployContracts.ts
@@ -429,7 +429,7 @@ async function main() {
                         0, // unused parameter
                     ],
                     {
-                        initializer: "initialize",
+                        initializer: "initialize(address,uint64,uint64,address,address,address,address,address,uint64,uint64)",
                         constructorArgs: [
                             polygonZkEVMGlobalExitRoot?.target,
                             polTokenAddress,
