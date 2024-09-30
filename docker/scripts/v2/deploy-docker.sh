1987a3bc9d9879d54abd6a600db8ad6e43be076c
Ignasi <ignasi@polygon.technology>
2024-09-30 15:16:52 +0200

    Fix script

--- docker/scripts/v2/deploy-docker.sh
+++ docker/scripts/v2/deploy-docker.sh
@@ -8,6 +8,7 @@ node docker/scripts/fund-accounts.js
 cp docker/scripts/v2/deploy_parameters_docker.json deployment/v2/deploy_parameters.json
 cp docker/scripts/v2/create_rollup_parameters_docker.json deployment/v2/create_rollup_parameters.json
 npm run deploy:testnet:v2:localhost
+rm -rf docker/deploymentOutput
 mkdir docker/deploymentOutput
 sudo mv deployment/v2/deploy_output.json docker/deploymentOutput
 sudo mv deployment/v2/genesis.json docker/deploymentOutput
@@ -16,5 +17,3 @@ sudo DEV_PERIOD=1 docker-compose -f docker/docker-compose.yml down
 sudo docker build -t hermeznetwork/geth-zkevm-contracts -f docker/Dockerfile .
 # Let it readable for the multiplatform build coming later!
 sudo chmod -R go+rxw docker/gethData
-# Run docker tests
-sh "$(dirname "$0")/tests-docker.sh"
--- docker/scripts/v2/tests-docker.sh
+++ docker/scripts/v2/tests-docker.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 # Set the -e option to stop the script if any command fails
 set -e
+# Build docker
+npm run dockerv2:contracts
+# Run docker tests
 # Run container
 docker run -p 8545:8545 -d --name docker_test hermeznetwork/geth-zkevm-contracts
 # Run docker tests
-npm run docker:tests
+npx hardhat test docker/docker-tests.test.ts --network localhost
 # stop container
 docker stop docker_test
 # remove container
