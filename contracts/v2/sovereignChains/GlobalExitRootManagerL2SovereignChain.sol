82f091191c2935d5d413cb78c758c6cc1df52d79
invocamanman <jesus.ligero@yahoo.es>
2024-10-03 04:43:44 +0200

    test soverign GER

--- contracts/v2/sovereignChains/GlobalExitRootManagerL2SovereignChain.sol
+++ contracts/v2/sovereignChains/GlobalExitRootManagerL2SovereignChain.sol
@@ -12,27 +12,88 @@ contract GlobalExitRootManagerL2SovereignChain is PolygonZkEVMGlobalExitRootL2 {
      */
     event InsertGlobalExitRoot(bytes32 indexed newGlobalExitRoot);
 
+    // Injected GER counter
+    uint256 public injectedGERCount;
+
     /**
      * @param _bridgeAddress PolygonZkEVMBridge contract address
      */
     constructor(
         address _bridgeAddress
-    ) PolygonZkEVMGlobalExitRootL2( _bridgeAddress) {}
+    ) PolygonZkEVMGlobalExitRootL2(_bridgeAddress) {}
 
     /**
      * @notice Insert a new global exit root
      * @param _newRoot new global exit root
      */
-    function insertGlobalExitRoot(
-        bytes32 _newRoot
-    ) external {
+    function insertGlobalExitRoot(bytes32 _newRoot) external {
         // Only allowed to be called by coinbase
-         if (block.coinbase != msg.sender) {
+        if (block.coinbase != msg.sender) {
             revert OnlyCoinbase();
         }
         // do not update timestamp if already set
         if (globalExitRootMap[_newRoot] == 0) {
-            globalExitRootMap[_newRoot] = block.timestamp;
+            globalExitRootMap[_newRoot] = ++injectedGERCount;
+            emit InsertGlobalExitRoot(_newRoot);
+        } else {
+            revert GlobalExitRootAlreadySet();
+        }
+    }
+
+    function checkInjectedGERsAndReturnLER(
+        uint256 previousInjectedGERCount,
+        bytes32[] memory injectedGERs
+    ) public view returns (bool success, bytes32 localExitRoot) {
+        // save on cache las injecterGerCount
+        uint256 cacheInjectedGERCount = injectedGERCount;
+
+        // previous injected GER must be equal or less than the last one
+        if (previousInjectedGERCount > cacheInjectedGERCount) {
+            return (false, bytes32(0));
+        }
+
+        // Compute injected GERs in the blockSpawn
+        uint256 injectedGERsNum = cacheInjectedGERCount -
+            previousInjectedGERCount;
+
+        uint256 currentInjectedGER = previousInjectedGERCount;
+
+        for (uint256 i = 0; i < injectedGERsNum; i++) {
+            // Point to the next GER
+            currentInjectedGER++;
+
+            // Assert that the GER exist and matches the proper count
+            if (globalExitRootMap[injectedGERs[i]] != currentInjectedGER) {
+                return (false, bytes32(0));
+            }
+        }
+
+        if (currentInjectedGER == cacheInjectedGERCount) {
+            return (true, lastRollupExitRoot);
+        } else {
+            return (false, bytes32(0));
+        }
+    }
+
+    function checkGERsExistance(
+        bytes32[] calldata globalExitRoots
+    ) public view returns (bool success) {
+        for (uint256 i = 0; i < globalExitRoots.length; i++) {
+            if (globalExitRootMap[globalExitRoots[i]] == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * @notice Insert a new global exit root
+     * @param _newRoot new global exit root
+     */
+    function insertGlobalExitRoot_cheat(bytes32 _newRoot) external {
+        // do not update timestamp if already set
+        if (globalExitRootMap[_newRoot] == 0) {
+            globalExitRootMap[_newRoot] = ++injectedGERCount;
             emit InsertGlobalExitRoot(_newRoot);
         } else {
             revert GlobalExitRootAlreadySet();
---
+++ tools/deployGERL2Sovereign/deployVerifier.ts
@@ -0,0 +1,85 @@
+/* eslint-disable no-await-in-loop, no-use-before-define, no-lonely-if */
+/* eslint-disable no-console, no-inner-declarations, no-undef, import/no-unresolved */
+import {expect} from "chai";
+import path = require("path");
+import fs = require("fs");
+
+import * as dotenv from "dotenv";
+dotenv.config({path: path.resolve(__dirname, "../../../.env")});
+import {ethers, upgrades} from "hardhat";
+const deployParameters = require("./deploy_ger_parameters.json");
+
+async function main() {
+    // Load provider
+    let currentProvider = ethers.provider;
+    if (deployParameters.multiplierGas || deployParameters.maxFeePerGas) {
+        if (process.env.HARDHAT_NETWORK !== "hardhat") {
+            currentProvider = ethers.getDefaultProvider(
+                `https://${process.env.HARDHAT_NETWORK}.infura.io/v3/${process.env.INFURA_PROJECT_ID}`
+            ) as any;
+            if (deployParameters.maxPriorityFeePerGas && deployParameters.maxFeePerGas) {
+                console.log(
+                    `Hardcoded gas used: MaxPriority${deployParameters.maxPriorityFeePerGas} gwei, MaxFee${deployParameters.maxFeePerGas} gwei`
+                );
+                const FEE_DATA = new ethers.FeeData(
+                    null,
+                    ethers.parseUnits(deployParameters.maxFeePerGas, "gwei"),
+                    ethers.parseUnits(deployParameters.maxPriorityFeePerGas, "gwei")
+                );
+
+                currentProvider.getFeeData = async () => FEE_DATA;
+            } else {
+                console.log("Multiplier gas used: ", deployParameters.multiplierGas);
+                async function overrideFeeData() {
+                    const feedata = await ethers.provider.getFeeData();
+                    return new ethers.FeeData(
+                        null,
+                        ((feedata.maxFeePerGas as bigint) * BigInt(deployParameters.multiplierGas)) / 1000n,
+                        ((feedata.maxPriorityFeePerGas as bigint) * BigInt(deployParameters.multiplierGas)) / 1000n
+                    );
+                }
+                currentProvider.getFeeData = overrideFeeData;
+            }
+        }
+    }
+
+    // Load deployer
+    let deployer;
+    if (deployParameters.deployerPvtKey) {
+        deployer = new ethers.Wallet(deployParameters.deployerPvtKey, currentProvider);
+    } else if (process.env.MNEMONIC) {
+        deployer = ethers.HDNodeWallet.fromMnemonic(
+            ethers.Mnemonic.fromPhrase(process.env.MNEMONIC),
+            "m/44'/60'/0'/0/0"
+        ).connect(currentProvider);
+    } else {
+        [deployer] = await ethers.getSigners();
+    }
+
+    console.log("--> Deploying with: ", deployer.address);
+
+    const GlobalExitRootSovereignFactory = await ethers.getContractFactory(
+        "GlobalExitRootManagerL2SovereignChain",
+        deployer
+    );
+
+    const bridgeAddress = ethers.ZeroAddress;
+    const globalExitRootSovereignContract = await GlobalExitRootSovereignFactory.deploy(bridgeAddress);
+    await globalExitRootSovereignContract.waitForDeployment();
+
+    // print contract address deployed
+    console.log("\n#######################");
+    console.log("globalExitRootSovereignContract deployed to:", globalExitRootSovereignContract.target);
+    console.log("#######################\n");
+    // print verification command line
+    console.log("you can verify the contract address with:");
+    console.log(
+        `npx hardhat verify --constructor-args upgrade/arguments.js ${globalExitRootSovereignContract.target} --network ${process.env.HARDHAT_NETWORK}\n`
+    );
+    console.log("Copy the following constructor arguments on: upgrade/arguments.js \n", [bridgeAddress]);
+}
+
+main().catch((e) => {
+    console.error(e);
+    process.exit(1);
+});
