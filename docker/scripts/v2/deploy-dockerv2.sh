0225d1561a13360f29ce211484bbd6cc6ee2d11f
Ignasi <ignasi@polygon.technology>
2024-10-17 16:08:25 +0200

    Update bridge at create rollup for vanilla clients

--- docker/scripts/v2/deploy-dockerv2.sh
+++ docker/scripts/v2/deploy-dockerv2.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 sudo rm -rf docker/gethData/geth_data
 sudo DEV_PERIOD=1 docker compose -f docker/docker-compose.yml up -d geth
 sleep 5
@@ -6,6 +7,7 @@ node docker/scripts/fund-accounts.js
 cp docker/scripts/v2/deploy_parameters_docker.json deployment/v2/deploy_parameters.json
 cp docker/scripts/v2/create_rollup_parameters_docker.json deployment/v2/create_rollup_parameters.json
 npm run deploy:testnet:v2:localhost
+rm -rf docker/deploymentOutput
 mkdir docker/deploymentOutput
 sudo mv deployment/v2/deploy_output.json docker/deploymentOutput
 sudo mv deployment/v2/genesis.json docker/deploymentOutput
