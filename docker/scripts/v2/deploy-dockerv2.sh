a7c6ca0a47c783b2ce242c70227c71735baa3b96
Ignasi <ignasi@polygon.technology>
2024-10-24 13:11:37 +0200

    Improve WETH comments at bridge contract

--- docker/scripts/v2/deploy-dockerv2.sh
+++ docker/scripts/v2/deploy-dockerv2.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Set the -e option to stop the script if any command fails
 set -e
-sudo rm -rf docker/gethData/geth_data
-sudo DEV_PERIOD=1 docker compose -f docker/docker-compose.yml up -d geth
+rm -rf docker/gethData/geth_data
+DEV_PERIOD=1 docker compose -f docker/docker-compose.yml up -d geth
 sleep 5
 node docker/scripts/fund-accounts.js
 cp docker/scripts/v2/deploy_parameters_docker.json deployment/v2/deploy_parameters.json
@@ -10,10 +10,10 @@ cp docker/scripts/v2/create_rollup_parameters_docker.json deployment/v2/create_r
 npm run deploy:testnet:v2:localhost
 rm -rf docker/deploymentOutput
 mkdir docker/deploymentOutput
-sudo mv deployment/v2/deploy_output.json docker/deploymentOutput
-sudo mv deployment/v2/genesis.json docker/deploymentOutput
-sudo mv deployment/v2/create_rollup_output.json docker/deploymentOutput
-sudo DEV_PERIOD=1 docker compose -f docker/docker-compose.yml down
-sudo docker build -t hermeznetwork/geth-zkevm-contracts -f docker/Dockerfile .
+mv deployment/v2/deploy_output.json docker/deploymentOutput
+mv deployment/v2/genesis.json docker/deploymentOutput
+mv deployment/v2/create_rollup_output.json docker/deploymentOutput
+DEV_PERIOD=1 docker compose -f docker/docker-compose.yml down
+docker build -t hermeznetwork/geth-zkevm-contracts -f docker/Dockerfile .
 # Let it readable for the multiplatform build coming later!
-sudo chmod -R go+rxw docker/gethData
+chmod -R go+rxw docker/gethData
