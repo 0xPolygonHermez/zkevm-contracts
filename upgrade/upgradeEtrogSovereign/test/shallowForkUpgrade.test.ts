794be0432505ad62bb04a443e1f9bf86a427e174
Laia Soler <laia@polygon.technology>
2025-09-18 16:58:10 +0200

    upgrade script etrog -> sovereign

---
+++ upgrade/upgradeEtrogSovereign/test/shallowForkUpgrade.test.ts
@@ -0,0 +1,158 @@
+/* eslint-disable no-await-in-loop, no-use-before-define, no-lonely-if */
+/* eslint-disable no-console, no-inner-declarations, no-undef, import/no-unresolved */
+import { expect } from 'chai';
+import path = require('path');
+
+import * as dotenv from 'dotenv';
+import { ethers, upgrades } from 'hardhat';
+import { time, reset, setBalance, mine } from '@nomicfoundation/hardhat-network-helpers';
+import {
+    PolygonZkEVMTimelock,
+    AgglayerBridge,
+    AgglayerGERL2,
+    AgglayerBridgeL2,
+} from '../../../typechain-types';
+import upgradeParams from '../upgrade_parameters.json';
+import upgradeOutput from '../upgrade_output.json';
+import { logger } from '../../../src/logger';
+import { checkParams } from '../../../src/utils';
+import { ProxyAdmin } from '../../../typechain-types/@openzeppelin/contracts4/proxy/transparent';
+
+dotenv.config({ path: path.resolve(__dirname, '../../.env') });
+
+dotenv.config({ path: path.resolve(__dirname, '../../.env') });
+
+async function main() {
+    // Asser upgrade version
+    const BRIDGE_VERSION = 'v1.1.0';
+    const GER_VERSION = 'v1.0.0';
+    const mandatoryParameters = ['timelockAdminAddress', 'rpc'];
+    checkParams(upgradeParams.forkParams, mandatoryParameters);
+    const rpc =
+        typeof upgradeParams.forkParams.rpc === 'undefined'
+            ? `https://${upgradeParams.forkParams.network}.infura.io/v3/${process.env.INFURA_PROJECT_ID}`
+            : upgradeParams.forkParams.rpc;
+
+    // hard fork
+    logger.info(`Shallow forking ${rpc}`);
+    try {
+        await reset(rpc, upgradeOutput.implementationDeployBlockNumber + 1);
+        await mine();
+    } catch (e) {
+        console.log(e);
+    }
+
+    logger.info(`Shallow forked block number: ${await ethers.provider.getBlockNumber()}`);
+    const forkedBlock = await ethers.provider.getBlockNumber();
+
+    // If forked block is lower than implementation deploy block, wait until it is reached
+    while (forkedBlock <= upgradeOutput.implementationDeployBlockNumber) {
+        logger.info(
+            `Forked block is ${forkedBlock}, waiting until ${upgradeOutput.implementationDeployBlockNumber}, wait 1 minute...`,
+        );
+        await new Promise((r) => {
+            setTimeout(r, 60000);
+        });
+        logger.info('Retrying fork...');
+        await reset(rpc);
+    }
+    logger.info('Shallow fork Succeed!');
+    const bridgeImplAddress = upgradeOutput.bridgeImplementationAddress;
+    const gerImplAddress = upgradeOutput.GERImplementationAddress;
+
+    // Check implementations exist
+    const bridgeImpCode = await ethers.provider.getCode(bridgeImplAddress);
+    expect(bridgeImpCode.length).to.be.greaterThan(2);
+    const gerImpCode = await ethers.provider.getCode(gerImplAddress);
+    expect(gerImpCode.length).to.be.greaterThan(2);
+
+    // Check timelock contract
+    const proxyAdminAddress = await upgrades.erc1967.getAdminAddress(upgradeParams.bridgeL2);
+    const proxyAdminFactory = await ethers.getContractFactory(
+        '@openzeppelin/contracts4/proxy/transparent/ProxyAdmin.sol:ProxyAdmin',
+    );
+    const proxyAdmin = proxyAdminFactory.attach(proxyAdminAddress) as ProxyAdmin;
+    const ownerAddress = await proxyAdmin.owner();
+    expect(upgradeOutput.timelockContractAddress).to.be.equal(ownerAddress);
+    logger.info('✓ proxy admin role is same as upgrade output file timelock address');
+
+    // Check proposed timelock admin address has proposer and executor role
+    const timelockContractFactory = await ethers.getContractFactory('PolygonZkEVMTimelock');
+    const timelockContract = (await timelockContractFactory.attach(ownerAddress)) as PolygonZkEVMTimelock;
+    const PROPOSER_ROLE = ethers.id('PROPOSER_ROLE');
+    const EXECUTOR_ROLE = ethers.id('EXECUTOR_ROLE');
+    const proposerRoleAddress = upgradeParams.forkParams.timelockAdminAddress;
+    const hasProposerRole = await timelockContract.hasRole(PROPOSER_ROLE, proposerRoleAddress);
+    const hasExecutorRole = await timelockContract.hasRole(EXECUTOR_ROLE, proposerRoleAddress);
+
+    if (!hasProposerRole || !hasExecutorRole) {
+        throw new Error('Timelock admin address does not have proposer and executor role');
+    }
+    logger.info(`Proposer/executor timelock role address: ${proposerRoleAddress}`);
+    await ethers.provider.send('hardhat_impersonateAccount', [proposerRoleAddress]);
+    const proposerRoleSigner = await ethers.getSigner(proposerRoleAddress as any);
+    await setBalance(proposerRoleAddress, 100n ** 18n);
+    logger.info(`✓ Funded proposer account ${proposerRoleAddress}`);
+
+    // get current storage values before upgrade
+    const polygonZkEVMBridgeL2 = await ethers.getContractFactory('AgglayerBridge');
+    const bridgeOldContract = polygonZkEVMBridgeL2.attach(upgradeParams.bridgeL2) as AgglayerBridge;
+    const bridgeLastUpdatedDepositCount = await bridgeOldContract.lastUpdatedDepositCount();
+    const bridgeRollupManager = await bridgeOldContract.polygonRollupManager();
+    const bridgeGasTokenAddress = await bridgeOldContract.gasTokenAddress();
+    const bridgeGasTokenNetwork = await bridgeOldContract.gasTokenNetwork();
+    const bridgeGasTokenMetadata = await bridgeOldContract.gasTokenMetadata();
+
+    // Send schedule transaction
+    const txScheduleUpgrade = {
+        to: upgradeOutput.timelockContractAddress,
+        data: upgradeOutput.scheduleData,
+    };
+    await (await proposerRoleSigner.sendTransaction(txScheduleUpgrade)).wait();
+
+    logger.info('✓ Sent schedule transaction');
+
+    // Increase time to bypass the timelock delay
+    const timelockDelay = upgradeOutput.decodedScheduleData.delay;
+    await time.increase(Number(timelockDelay));
+    logger.info(`✓ Increase time ${timelockDelay} seconds to bypass timelock delay`);
+
+    // Send execute transaction
+    const txExecuteUpgrade = {
+        to: upgradeOutput.timelockContractAddress,
+        data: upgradeOutput.executeData,
+    };
+    await (await proposerRoleSigner.sendTransaction(txExecuteUpgrade)).wait();
+    logger.info(`✓ Sent execute transaction`);
+
+    // Check ger params after upgrade
+    const agglayerGERL2Factory = await ethers.getContractFactory('AgglayerGERL2');
+    const gerL2Contract = agglayerGERL2Factory.attach(upgradeParams.gerL2) as AgglayerGERL2;
+    expect(await gerL2Contract.globalExitRootUpdater()).to.equal(upgradeParams.globalExitRootUpdater);
+    expect(await gerL2Contract.globalExitRootRemover()).to.equal(upgradeParams.globalExitRootRemover);
+    expect(await gerL2Contract.GER_SOVEREIGN_VERSION()).to.equal(GER_VERSION);
+
+    logger.info(`✓ Checked AgglayerGERL2 contract storage parameters`);
+
+    // Check bridge params after upgrade
+    const bridgeFactory = await ethers.getContractFactory('AgglayerBridgeL2');
+    const bridgeContract = bridgeFactory.attach(upgradeParams.bridgeL2) as AgglayerBridgeL2;
+    expect(await bridgeContract.BRIDGE_SOVEREIGN_VERSION()).to.equal(BRIDGE_VERSION);
+    expect(await bridgeContract.globalExitRootManager()).to.equal(upgradeParams.gerL2);
+    expect(await bridgeContract.lastUpdatedDepositCount()).to.equal(bridgeLastUpdatedDepositCount);
+    expect(await bridgeContract.polygonRollupManager()).to.equal(bridgeRollupManager);
+    expect(await bridgeContract.gasTokenAddress()).to.equal(bridgeGasTokenAddress);
+    expect(await bridgeContract.gasTokenNetwork()).to.equal(bridgeGasTokenNetwork);
+    expect(await bridgeContract.gasTokenMetadata()).to.equal(bridgeGasTokenMetadata);
+    expect(await bridgeContract.proxiedTokensManager()).to.equal(upgradeParams.proxiedTokensManagerAddress);
+    expect(await bridgeContract.emergencyBridgePauser()).to.equal(upgradeParams.emergencyBridgePauserAddress);
+    expect(await bridgeContract.emergencyBridgeUnpauser()).to.equal(upgradeParams.emergencyBridgeUnpauserAddress);
+
+    logger.info(`✓ Checked AgglayerBridgeL2 contract storage parameters`);
+    logger.info('Finished shallow fork upgrade');
+}
+
+main().catch((e) => {
+    console.error(e);
+    process.exit(1);
+});
---
+++ upgrade/upgradeEtrogSovereign/upgradeEtrogToSovereign.ts
@@ -0,0 +1,196 @@
+/* eslint-disable no-await-in-loop, no-use-before-define, no-lonely-if */
+/* eslint-disable no-console, no-inner-declarations, no-undef, import/no-unresolved */
+import { expect } from 'chai';
+import path = require('path');
+import fs = require('fs');
+import { utils } from 'ffjavascript';
+import { ethers, upgrades } from 'hardhat';
+import * as dotenv from 'dotenv';
+import { logger } from '../../src/logger';
+import { TimelockController } from '../../typechain-types';
+import { genTimelockOperation, decodeScheduleData } from '../utils';
+import { checkParams, getDeployerFromParameters, getProviderAdjustingMultiplierGas } from '../../src/utils';
+
+import upgradeParameters from './upgrade_parameters.json';
+
+dotenv.config({ path: path.resolve(__dirname, '../../.env') });
+
+const pathOutputJson = path.join(__dirname, './upgrade_output.json');
+
+async function main() {
+    let outputJson = {};
+
+    /*
+     * Check upgrade parameters
+     * Check that every necessary parameter is fulfilled
+     */
+    const mandatoryUpgradeParameters = [
+        'bridgeManager',
+        'bridgeL2',
+        'gerL2',
+        'proxiedTokensManagerAddress',
+        'emergencyBridgePauserAddress',
+        'emergencyBridgeUnpauserAddress',
+        'globalExitRootUpdater',
+        'globalExitRootRemover',
+    ];
+    checkParams(upgradeParameters, mandatoryUpgradeParameters);
+    const salt = upgradeParameters.timelockSalt || ethers.ZeroHash;
+
+    const {
+        bridgeManager,
+        proxiedTokensManagerAddress,
+        emergencyBridgePauserAddress,
+        emergencyBridgeUnpauserAddress,
+        bridgeL2,
+        gerL2,
+        globalExitRootUpdater,
+        globalExitRootRemover,
+    } = upgradeParameters;
+
+    // Load provider
+    const currentProvider = getProviderAdjustingMultiplierGas(upgradeParameters, ethers);
+
+    // Load deployer
+    const deployer = await getDeployerFromParameters(currentProvider, upgradeParameters, ethers);
+    logger.info(`Deploying implementation with: ${deployer.address}`);
+
+    // Force import hardhat manifest
+    logger.info('Force import hardhat manifest');
+    // As this contract is deployed in the genesis of a L2 network, no open zeppelin network file is created, we need to force import it
+    const bridgeFactory = await ethers.getContractFactory('AgglayerBridgeL2FromEtrog', deployer);
+    await upgrades.forceImport(bridgeL2, bridgeFactory, {
+        constructorArgs: [],
+        kind: 'transparent',
+    });
+    const gerFactory = await ethers.getContractFactory('AgglayerGERL2', deployer);
+    await upgrades.forceImport(gerL2, gerFactory, {
+        constructorArgs: [bridgeL2],
+        kind: 'transparent',
+    });
+
+    // get proxy admin and timelock
+    logger.info('Get proxy admin information');
+    // Get proxy admin
+    const proxyAdmin = await upgrades.admin.getInstance();
+
+    // Assert correct admin
+    expect(await upgrades.erc1967.getAdminAddress(gerL2 as string)).to.be.equal(proxyAdmin.target);
+    expect(await upgrades.erc1967.getAdminAddress(bridgeL2 as string)).to.be.equal(proxyAdmin.target);
+
+    const timelockAddress = await proxyAdmin.owner();
+
+    // load timelock
+    const timelockContractFactory = await ethers.getContractFactory('PolygonZkEVMTimelock', deployer);
+    const timelockContract = (await timelockContractFactory.attach(timelockAddress)) as TimelockController;
+    // take params delay, or minimum timelock dela
+    const timelockDelay = upgradeParameters.timelockDelay || (await timelockContract.getMinDelay());
+
+    // Upgrade AgglayerBridge -> AgglayerBridgeL2FromEtrog
+    const impBridge = await upgrades.prepareUpgrade(bridgeL2, bridgeFactory, {
+        unsafeAllow: ['constructor', 'missing-initializer', 'missing-initializer-call'],
+        redeployImplementation: 'always',
+    });
+
+    logger.info('#######################\n');
+    logger.info(`Polygon sovereign bridge implementation deployed at: ${impBridge}`);
+
+    // Upgrade AgglayerGER --> AgglayerGERL2
+    const impGER = await upgrades.prepareUpgrade(gerL2, gerFactory, {
+        unsafeAllow: ['constructor', 'missing-initializer', 'missing-initializer-call'],
+        redeployImplementation: 'always',
+        constructorArgs: [bridgeL2],
+    });
+
+    logger.info(`Polygon sovereign GER implementation deployed at: ${impGER}`);
+
+    // Create schedule and execute operation
+    logger.info('Create schedule and execute operation');
+    logger.info('Operation Bridge');
+    const operationBridge = genTimelockOperation(
+        proxyAdmin.target,
+        0, // value
+        proxyAdmin.interface.encodeFunctionData('upgradeAndCall', [
+            bridgeL2,
+            impBridge,
+            bridgeFactory.interface.encodeFunctionData('initializeBridgeZkEVM(address,address,address,address)', [
+                bridgeManager,
+                emergencyBridgePauserAddress,
+                emergencyBridgeUnpauserAddress,
+                proxiedTokensManagerAddress,
+            ]),
+        ]), // data
+        ethers.ZeroHash, // predecessor
+        salt, // salt
+    );
+    logger.info('Operation GER');
+    const operationGER = genTimelockOperation(
+        proxyAdmin.target,
+        0, // value
+        proxyAdmin.interface.encodeFunctionData('upgradeAndCall', [
+            gerL2,
+            impGER,
+            gerFactory.interface.encodeFunctionData('initialize(address,address)', [
+                globalExitRootUpdater,
+                globalExitRootRemover,
+            ]),
+        ]), // data
+        ethers.ZeroHash, // predecessor
+        salt, // salt
+    );
+    logger.info('scheduleData & executeData');
+    // Schedule operation
+    const scheduleData = timelockContractFactory.interface.encodeFunctionData('scheduleBatch', [
+        [operationBridge.target, operationGER.target],
+        [operationBridge.value, operationGER.value],
+        [operationBridge.data, operationGER.data],
+        ethers.ZeroHash, // predecessor
+        salt, // salt
+        timelockDelay,
+    ]);
+
+    // Execute operation
+    const executeData = timelockContractFactory.interface.encodeFunctionData('executeBatch', [
+        [operationBridge.target, operationGER.target],
+        [operationBridge.value, operationGER.value],
+        [operationBridge.data, operationGER.data],
+        ethers.ZeroHash, // predecessor
+        salt, // salt
+    ]);
+
+    logger.info({ scheduleData });
+    logger.info({ executeData });
+
+    // Get current block number, used in the shallow fork tests
+    const blockNumber = await ethers.provider.getBlockNumber();
+    outputJson = {
+        scheduleData,
+        executeData,
+        timelockContractAddress: timelockAddress,
+        implementationDeployBlockNumber: blockNumber,
+        bridgeImplementationAddress: impBridge,
+        GERImplementationAddress: impGER,
+        inputs: {
+            bridgeManager,
+            proxiedTokensManagerAddress,
+            emergencyBridgePauserAddress,
+            emergencyBridgeUnpauserAddress,
+            globalExitRootUpdater,
+            globalExitRootRemover,
+            bridgeL2,
+            gerL2,
+        },
+    };
+
+    // Decode the scheduleData for better readability
+    const objectDecoded = await decodeScheduleData(scheduleData, proxyAdmin);
+    (outputJson as any).decodedScheduleData = objectDecoded;
+
+    fs.writeFileSync(pathOutputJson, JSON.stringify(utils.stringifyBigInts(outputJson), null, 1));
+    logger.info(`Output saved to: ${pathOutputJson}`);
+}
+
+main().catch((e) => {
+    console.error(e);
+    process.exit(1);
+});
