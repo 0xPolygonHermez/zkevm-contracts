4834c3ec0d7c39e63b4d3fdb3a0124480abddd75
alrevuelta <alvrevuelta@gmail.com>
2025-12-03 10:24:00 +0100

    Remove sudo docker deployments

--- docker/scripts/v2/deploy-dockerv2-all.sh
+++ docker/scripts/v2/deploy-dockerv2-all.sh
@@ -3,15 +3,15 @@
 set -e
 # Define cleanup function
 cleanup() {
-    sudo DEV_PERIOD=1 docker compose -f docker/docker-compose.yml down
+    DEV_PERIOD=1 docker compose -f docker/docker-compose.yml down
 }
 # Configure "trap" in case there's an error
 trap cleanup ERR
 # remove old data
-sudo rm -rf docker/gethData/geth_data
+rm -rf docker/gethData/geth_data
 [ -f deployment/v2/create_rollup_output_* ] && rm ./deployment/v2/create_rollup_output_*
 # start geth
-sudo DEV_PERIOD=1 docker compose -f docker/docker-compose.yml up -d geth
+DEV_PERIOD=1 docker compose -f docker/docker-compose.yml up -d geth
 sleep 5
 # fund accounts
 node docker/scripts/fund-accounts.ts
@@ -21,7 +21,7 @@ cp docker/scripts/v2/create_rollup_parameters_docker.json deployment/v2/create_r
 # deploy contracts & create rollup FEP
 npm run deploy:testnet:v2:localhost
 # remove old data
-sudo rm -rf docker/deploymentOutput
+rm -rf docker/deploymentOutput
 # create new folder for deployment output
 mkdir docker/deploymentOutput
 # move rollup output to output folder
@@ -51,10 +51,10 @@ npx hardhat run ./deployment/v2/4_createRollup.ts --network localhost
 # move rollup output to output folder
 mv ./deployment/v2/create_rollup_output_*.json ./docker/deploymentOutput/create_rollup_output_validium.json
 # move deployment output to output folder
-sudo mv deployment/v2/deploy_output.json docker/deploymentOutput
-sudo mv deployment/v2/genesis.json docker/deploymentOutput
-[ -f deployment/v2/genesis_sovereign.json ] && sudo mv deployment/v2/genesis_sovereign.json docker/deploymentOutput
-sudo DEV_PERIOD=1 docker compose -f docker/docker-compose.yml down
-sudo docker build -t hermeznetwork/geth-zkevm-contracts -f docker/Dockerfile .
+mv deployment/v2/deploy_output.json docker/deploymentOutput
+mv deployment/v2/genesis.json docker/deploymentOutput
+[ -f deployment/v2/genesis_sovereign.json ] && mv deployment/v2/genesis_sovereign.json docker/deploymentOutput
+DEV_PERIOD=1 docker compose -f docker/docker-compose.yml down
+docker build -t hermeznetwork/geth-zkevm-contracts -f docker/Dockerfile .
 # Let it readable for the multiplatform build coming later!
-sudo chmod -R go+rxw docker/gethData
+chmod -R go+rxw docker/gethData
--- docker/scripts/v2/deploy-dockerv2.sh
+++ docker/scripts/v2/deploy-dockerv2.sh
@@ -3,25 +3,25 @@
 set -e
 # Define cleanup function
 cleanup() {
-    sudo DEV_PERIOD=1 docker compose -f docker/docker-compose.yml down
+    DEV_PERIOD=1 docker compose -f docker/docker-compose.yml down
 }
 # Configure "trap" in case there's an error
 trap cleanup ERR
 
-sudo rm -rf docker/gethData/geth_data
-sudo DEV_PERIOD=1 docker compose -f docker/docker-compose.yml up -d geth
+rm -rf docker/gethData/geth_data
+DEV_PERIOD=1 docker compose -f docker/docker-compose.yml up -d geth
 sleep 5
 node docker/scripts/fund-accounts.ts
 cp docker/scripts/v2/deploy_parameters_docker.json deployment/v2/deploy_parameters.json
 cp docker/scripts/v2/create_rollup_parameters_docker.json deployment/v2/create_rollup_parameters.json
 npm run deploy:testnet:v2:localhost
-sudo rm -rf docker/deploymentOutput
+rm -rf docker/deploymentOutput
 mkdir docker/deploymentOutput
-sudo mv deployment/v2/deploy_output.json docker/deploymentOutput
-sudo mv deployment/v2/genesis.json docker/deploymentOutput
-[ -f deployment/v2/genesis_sovereign.json ] && sudo mv deployment/v2/genesis_sovereign.json docker/deploymentOutput
-sudo mv deployment/v2/create_rollup_output_* docker/deploymentOutput/create_rollup_output.json
-sudo DEV_PERIOD=1 docker compose -f docker/docker-compose.yml down
-sudo docker build -t hermeznetwork/geth-zkevm-contracts -f docker/Dockerfile .
+mv deployment/v2/deploy_output.json docker/deploymentOutput
+mv deployment/v2/genesis.json docker/deploymentOutput
+[ -f deployment/v2/genesis_sovereign.json ] && mv deployment/v2/genesis_sovereign.json docker/deploymentOutput
+mv deployment/v2/create_rollup_output_* docker/deploymentOutput/create_rollup_output.json
+DEV_PERIOD=1 docker compose -f docker/docker-compose.yml down
+docker build -t hermeznetwork/geth-zkevm-contracts -f docker/Dockerfile .
 # Let it readable for the multiplatform build coming later!
-sudo chmod -R go+rxw docker/gethData
+chmod -R go+rxw docker/gethData
